25636acc0bdb7d2d6bfe1a86fef8254e4decc406
Gianni0211 <gianni.mondello@live.it>
2023-04-03 17:48:39 +0200

    iniziati i primi rest e models per user

--- index.js
+++ index.js
@@ -1,11 +1,83 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./models/userModel");
+
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/users", async (req, res) => {
+  try {
+    const user = await User.find({});
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json({ message: error.message });
+  }
 });
+
+app.get("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/user", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndUpdate(id, req.body);
+    if (!user) {
+      res.status(404).json({ message: "Couldn't find User with id: " + id });
+    }
+    const updatedUser = await User.findById(id);
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      res.status(404).json({ message: "Couldn't find User with id: " + id });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect("mongodb://localhost:27017/?directConnection=true")
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
